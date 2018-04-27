cf62b2c44fcd0e2270c9041cd670d90e802df95a
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-04-27 17:14:49 +0300

    Contacts page works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {SettingsPage} from "../pages/settings/settings";
-import { WeatherProvider } from '../providers/weather/weather';
+import {WeatherProvider} from '../providers/weather/weather';
 import {HttpClientModule} from "@angular/common/http";
+import {IonicStorageModule} from "@ionic/storage";
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     HttpClientModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {WeatherProvider} from "../../providers/weather/weather";
+import {Storage} from "@ionic/storage";
 
 @Component({
   selector: 'page-home',
@@ -14,18 +15,25 @@ export class HomePage {
     state: string
   };
 
-  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
-
+  constructor(public navCtrl: NavController,
+              private weatherProvider: WeatherProvider,
+              private storage: Storage) {
   }
 
   ionViewWillEnter() {
-    this.location = {
-      city: "Obninsk",
-      state: "RU"
-    };
+    this.storage.get("location").then(value => {
+      if (value) {
+        this.location = JSON.parse(value);
+      } else {
+        this.location = {
+          city: "Obninsk",
+          state: "RU"
+        };
+      }
 
-    this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe((weather: any) => {
-      this.weather = weather.current_observation;
-    })
+      this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe((weather: any) => {
+        this.weather = weather.current_observation;
+      })
+    });
   }
 }
