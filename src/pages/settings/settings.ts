8a78b158ba69f019c116e2df20932b514064b901
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-04-27 18:22:16 +0300

    Save location in settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
+import {Storage} from "@ionic/storage";
+import {HomePage} from "../home/home";
 
 @Component({
   selector: 'page-settings',
@@ -7,7 +9,35 @@ import {NavController, NavParams} from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  city: string;
+  state: string;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private storage: Storage) {
+
+  }
+
+  ionViewWillEnter() {
+    this.storage.get("location").then(value => {
+      if (value) {
+        let location = JSON.parse(value);
+        this.city = location.city;
+        this.state = location.state;
+      } else {
+
+      }
+    });
+  }
+
+  saveSettings() {
+    this.saveLocation();
+    this.navCtrl.push(HomePage);
+  }
+
+  saveLocation() {
+    let location = {city: this.city, state: this.state};
+    this.storage.set("location", JSON.stringify(location));
   }
 
 }
